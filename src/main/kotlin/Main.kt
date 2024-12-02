7e3bd090737b0c599707b6b7289dde594e8ee314
troy <troy@unverdruss.net>
2024-12-01 20:35:18 -0800

    add descriptors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ package net.unverdruss
 fun main() {
     val d = Day01()
     val result1 = d.part1()
-    println("Part 1: $result1")
+    println("Part 1, total distance between lists: $result1")
     val result2 = d.part2()
-    println("Part 2: $result2")
+    println("Part 2, similarity score: $result2")
 }
