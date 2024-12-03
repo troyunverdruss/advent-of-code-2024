fa25fb169e3f6de68fe076e5ff2771fbff5f1274
troy <troy@unverdruss.net>
2024-12-03 14:54:22 -0800

    day2

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -7,14 +7,133 @@ class Day02 {
     fun part1(): Long {
         val reports = loadReports()
         println("reports = ${reports}")
-        return reports.count { isReportSafe(it) }.toLong()
+        return reports.count { isReportSafePart1(it) }.toLong()
     }
 
     fun part2(): Long {
-        TODO("not done")
+        val reports = loadReports()
+
+        val totallyValid = reports.count { isReportSafePart1(it) }
+        val provisionallyValid = reports.filter { !isReportSafePart1(it) }
+            .map { report ->
+                (0..report.lastIndex).any {index ->
+                    val mutableReport = report.toMutableList()
+                    mutableReport.removeAt(index)
+                    isReportSafePart1(mutableReport)
+                }
+            }
+            .count { it }
+        return (totallyValid + provisionallyValid).toLong()
     }
 
-    fun isReportSafe(report: List<Long>): Boolean {
+    fun testForErrors(report: List<Long>) {
+
+        println("Testing ${report}")
+
+        val myPrediction = isReportSafe(report, enableDampener = true)
+        println("  Prediction: $myPrediction")
+        println("  Result with full list: ${isReportSafePart1(report)}")
+        println("  Starting to drop entries to see if one might be valid")
+        (0..report.lastIndex).forEach {index ->
+            val mutableReport = report.toMutableList()
+            mutableReport.removeAt(index)
+            val r = isReportSafePart1(mutableReport)
+            println("    Drop $index, list: $mutableReport, result: $r")
+        }
+
+//        val actual = isReportSafePart1(report)
+//
+//        val actuals = (0..report.lastIndex).any {
+//            val mutableReport = report.toMutableList()
+//            mutableReport.removeAt(it)
+//            val r = isReportSafePart1(mutableReport)
+////            println("${mutableReport} => $r")
+//            r
+//        }
+//
+////        println(myPrediction)
+////        println(actual)
+////        println(actuals)
+//
+//        if (myPrediction == actual) {
+//            println("Always correct: $report [$myPrediction == $actual]")
+//        } else {
+//            if (myPrediction == actuals) {
+//                println("Correct when dropping 1: $report [$myPrediction == $actuals]")
+//            } else {
+//                println("Mismatch when dropping 1: $report [$myPrediction == $actuals]")
+//            }
+//        }
+//        if (!(myPrediction == actual || myPrediction == actuals)) {
+//            println("Not correct: ${report}. [pred: ${myPrediction}, act: $actuals]")
+//        }
+    }
+
+    fun isReportSafe(report: List<Long>, enableDampener: Boolean = false): Boolean {
+        return if (enableDampener) {
+            isReportSafeTry2(report, enableDampener) || isReportSafeTry2(report.slice(1..report.lastIndex)) || isReportSafeTry2(report.drop(1))
+        } else {
+            isReportSafeTry2(report)
+        }
+    }
+
+    private fun isReportSafeTry2(report: List<Long>, enableDampener: Boolean = false): Boolean {
+        var errors = 0
+        var last: Long? = null
+        var dir: Direction? = null
+
+        for ((index, level) in report.withIndex()) {
+            if (last == null) {
+                last = level
+                continue
+            } else if (level == last) {
+                errors += 1
+                continue
+            } else if (abs(level - last) > 3) {
+                errors += 1
+                continue
+            } else if (dir == null) {
+                dir = if (level - last > 0) {
+                    Direction.UP
+                } else {
+                    Direction.DOWN
+                }
+                last = level
+            } else {
+                if (dir == Direction.UP) {
+                    if (level < last) {
+                        errors += 1
+                        continue
+                    } else if (level - last > 3) {
+                        errors += 1
+                        continue
+                    }
+                } else {
+                    if (level > last) {
+                        errors += 1
+                        continue
+                    } else if (last - level > 3) {
+                        errors += 1
+                        continue
+                    }
+                }
+                last = level
+            }
+        }
+
+        val safe = errors == 0 || (errors == 1 && enableDampener)
+
+//        if (safe) {
+//            println("safe: ${report}")
+//        } else {
+//            println("unsafe: ${report}")
+//
+//        }
+
+        return safe
+    }
+
+    fun isReportSafePart1(report: List<Long>): Boolean {
         var safe = true
         var last: Long? = null
         var dir: Direction? = null
@@ -58,15 +177,16 @@ class Day02 {
             }
         }
 
-        if (safe) {
-            println("safe: ${report}")
-        } else {
-            println("unsafe: ${report}")
-
-        }
+//        if (safe) {
+//            println("safe: ${report}")
+//        } else {
+//            println("unsafe: ${report}")
+//
+//        }
 
         return safe
     }
+
 
     private enum class Direction { UP, DOWN }
 
--- src/test/kotlin/Day02Test.kt
+++ src/test/kotlin/Day02Test.kt
@@ -24,6 +24,17 @@ class Day02Test {
         Pair(true, listOf(1L, 3, 2, 4, 5)),
         Pair(true, listOf(8L, 6, 4, 4, 1)),
         Pair(true, listOf(1L, 3, 6, 7, 9)),
+
+        Pair(true, listOf(1L, 5, 2, 5)),
+        Pair(false, listOf(1L, 5, 1, 5)),
+        Pair(true, listOf(1L, 3, 1, 5)),
+
+        Pair(true, listOf(5L, 2, 5, 1)),
+        Pair(false, listOf(5L, 1, 5, 1)),
+        Pair(true, listOf(5L, 1, 3, 1)),
+
+        Pair(true, listOf(1L, 2, 3, 10)),
+        Pair(true, listOf(10L, 2, 3, 4)),
     )
 
     @Test(dataProvider = "reportsProviderPart1")
@@ -32,10 +43,22 @@ class Day02Test {
         assertEquals(resultAndReport.first, d.isReportSafe(resultAndReport.second))
     }
 
-    @Test(dataProvider = "reportsProvider2")
+    @Test(dataProvider = "reportsProviderPart2")
     fun `part2`(resultAndReport: Pair<Boolean, List<Long>>) {
         val d = Day02()
-        assertEquals(resultAndReport.first, d.isReportSafe(resultAndReport.second))
+        assertEquals(resultAndReport.first, d.isReportSafe(resultAndReport.second, enableDampener = true))
     }
 
+    @Test
+    fun `part2 manual `() {
+        val resultAndReport = Pair(true, listOf(5L, 1, 3, 1))
+        val d = Day02()
+        assertEquals(resultAndReport.first, d.isReportSafe(resultAndReport.second, enableDampener = true))
+    }
+
+    @Test
+    fun `part2 test for errors`() {
+        val d = Day02()
+        d.testForErrors(listOf(2, 2, 3, 5, 4, 8))
+    }
 }
