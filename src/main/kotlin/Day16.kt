9bc6cb03899e71e007d9ff133b38ce281206ede0
Troy Unverdruss <troy@unverdruss.net>
2024-12-27 13:18:31 -0800

    test case runs, but needs optimizing

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,12 +1,12 @@
 import Utils.Direction
 import Utils.Point
 import java.io.File
+import java.util.*
 
 class Day16 : Day {
     override fun part1(): Long {
         val grid = Utils.parseGrid(File("inputs/day16.txt").readLines())
         return computePart1(grid)
-
     }
 
     override fun part2(): Long {
@@ -19,10 +19,70 @@ class Day16 : Day {
         TODO("Not yet implemented")
     }
 
+    data class FindState(val pos: Point, val dir: Direction) {
+        var score: Long = 0
+        var prev: FindState? = null
+    }
+
     fun computePart1(grid: Map<Point, Char>): Long {
+        val start = grid.filter { it.value == 'S' }.keys.first()
+        val end = grid.filter { it.value == 'E' }.keys.first()
+        val visited = mutableSetOf<FindState>()
+        val startingState = FindState(start, Direction.RIGHT)
+
+        val toVisit = LinkedList(listOf(startingState))
+        val solutions = mutableListOf<FindState>()
+
+        while (toVisit.isNotEmpty()) {
+            val curr = toVisit.removeFirst()
+            visited.add(curr)
+
+            if (curr.pos == end) {
+                solutions.add(curr)
+                continue
+            }
+
+            listOf(
+                curr.pos + curr.dir.point to curr.dir,
+                curr.pos + Direction.turnLeft90(curr.dir).point to Direction.turnLeft90(curr.dir),
+                curr.pos + Direction.turnRight90(curr.dir).point to Direction.turnRight90(curr.dir)
+            ).forEach { (pos, dir) ->
+                val testState = FindState(pos, dir)
+
+                testState.score = curr.score + 1
+                if (dir != curr.dir) {
+                    testState.score += 1000
+                }
+                testState.prev = curr
+                if (grid[testState.pos] != '#') {
+                    val existing = visited.find { it == testState }
+                    if (existing == null || existing.score > testState.score) {
+                        toVisit.add(testState)
+                    }
+                }
+            }
+        }
+
+        val solution = solutions.minByOrNull { it.score }
+
+        // Print out solution
+        val path = mutableListOf<FindState>()
+        var curr = solution
+        while (curr != null) {
+            path.add(curr)
+            curr = curr.prev
+        }
+        path.reversed().forEach {
+            println("${it.pos}, ${it.dir}: ${it.score}")
+        }
+
+        return solution?.score ?: throw RuntimeException("no path found")
+    }
+
+    fun brokenComputePart1(grid: Map<Point, Char>): Long {
         val pathFinder = PathFinder(grid)
 //        val memoizer = Memoizer(pathFinder.findPath)
-        return pathFinder.findPath(FindPathArgs( pathFinder.start, Direction.LEFT, 0))
+        return pathFinder.findPath(FindPathArgs(pathFinder.start, Direction.LEFT, 0))
             ?: throw RuntimeException("no path found")
     }
 
@@ -59,11 +119,11 @@ class PathFinder(val grid: Map<Point, Char>) {
 //        if (visited.contains(Pair(next, dir))) {
 //            return null
 //        }
-        val straight = findPath(FindPathArgs( next, dir, score + 1))
+        val straight = findPath(FindPathArgs(next, dir, score + 1))
         // Left
-        val left = findPath(FindPathArgs( pos, Direction.turnLeft90(dir), score + 1000))
+        val left = findPath(FindPathArgs(pos, Direction.turnLeft90(dir), score + 1000))
         // Right
-        val right = findPath(FindPathArgs( pos, Direction.turnRight90(dir), score + 1000))
+        val right = findPath(FindPathArgs(pos, Direction.turnRight90(dir), score + 1000))
 
         return listOfNotNull(straight, left, right).minOrNull()
     }
